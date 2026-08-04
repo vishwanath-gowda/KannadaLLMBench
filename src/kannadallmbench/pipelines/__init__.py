3a891a6a6f978352f5003876420a71958162216f
Vishwanath K <vishwanathgowdak@gmail.com>
2026-08-04 15:03:22 -0700

    Expose RomanBench construction pipeline API

--- src/kannadallmbench/pipelines/__init__.py
+++ src/kannadallmbench/pipelines/__init__.py
@@ -2,12 +2,16 @@
 
 from .build import BuildConfig, build_jsonl_from_hf
 from .dedup import deduplicate_records
+from .romanbench import ConstructionStats, RomanBenchFilter, construct_candidate_dataset
 from .slice import slice_records
 from .transforms import normalize_record, normalize_text
 
 __all__ = [
     "BuildConfig",
+    "ConstructionStats",
+    "RomanBenchFilter",
     "build_jsonl_from_hf",
+    "construct_candidate_dataset",
     "deduplicate_records",
     "normalize_record",
     "normalize_text",
