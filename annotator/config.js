57d1445263865c776198e49187de5ba5adbf0277
Vishwanath K <vishwanathgowdak@gmail.com>
2026-08-04 17:51:22 -0700

    Configure annotator frontend

---
+++ annotator/config.js
@@ -0,0 +1,8 @@
+window.ROMANBENCH_CONFIG = {
+  // Paste the deployed Google Apps Script Web App URL here for production.
+  // Example: https://script.google.com/macros/s/AKfycb.../exec
+  apiUrl: "",
+  demoWhenUnconfigured: true,
+  requestTimeoutMs: 15000,
+  instructionsVersion: "romanbench-annotation-v1",
+};
