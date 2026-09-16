b0f76be5490b0777279b734b6ac4b3a1ee308e63
Vishwanath K <vishwanathgowdak@gmail.com>
2026-09-16 14:24:53 -0700

    Allow longer Apps Script auth cold start

--- annotator/config.js
+++ annotator/config.js
@@ -3,6 +3,7 @@ window.ROMANBENCH_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbxBsiWGoNt2Q9RqSkLSrfR44ihOprFiANaEaHbfyLgyalZAOzBa76Fd4GRU4qf_StOB8w/exec",
   demoWhenUnconfigured: true,
   requestTimeoutMs: 15000,
+  authTimeoutMs: 45000,
   prefetchCount: 5,
   refillThreshold: 2,
   instructionsVersion: "romanbench-annotation-v2",
