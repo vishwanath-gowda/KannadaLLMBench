8c0c08fd52ed44d8454a62ea7ccd11766337dae4
Vishwanath K <vishwanathgowdak@gmail.com>
2026-09-16 13:41:43 -0700

    Speed up RomanBench annotation with buffered task leasing
    * Add buffered task leasing to Apps Script backend
    
    * Buffer leased tasks for instant annotation transitions
    
    * Configure buffered task prefetch
    
    * Preconnect annotator to Apps Script hosts
    
    * Prevent repeated buffered tasks during async writes
    
    * Test RomanBench buffered annotation contract
    
    * Document buffered leasing architecture and pilot setup

--- annotator/config.js
+++ annotator/config.js
@@ -3,6 +3,8 @@ window.ROMANBENCH_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbzxAao8M8Mt-81wzCRKj1n_FIE37IMpJKvYCyaOYCD609hccH3YUyBUfMkjItPyQlEztg/exec",
   demoWhenUnconfigured: true,
   requestTimeoutMs: 15000,
+  prefetchCount: 5,
+  refillThreshold: 2,
   instructionsVersion: "romanbench-annotation-v2",
   termsVersion: "romanbench-validation-v1",
 };
